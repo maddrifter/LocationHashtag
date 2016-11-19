ab6cf40316612498ac6791d2e2025a0f1179fb68
KazuoOhara <kazuo.ohara@hotmail.com>
2016-11-19 22:53:26 +0100

    upload location hashtag project

---
+++ Hashtag.swift
@@ -0,0 +1,17 @@
+//
+//  Hashtag.swift
+//  LocationHashtag
+//
+//  Created by maddrifter on 6/30/16.
+//
+//
+
+import Foundation
+import CoreData
+
+
+class Hashtag: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
